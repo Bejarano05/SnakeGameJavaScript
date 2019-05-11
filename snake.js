88a6a9fa994a5eda70ca540ad95e86e74cfb60f4
Juan Antonio Bejarano Basulto <juanantonbejarano7@gmail.com>
2019-05-11 21:29:47 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,10 +1,7 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
-// create the unit
 const box = 32;
-
-// load images
 
 const ground = new Image();
 ground.src = "img/ground.png";
